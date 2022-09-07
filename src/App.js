c25c2282facea6d73c659a3c0520a6744d2e7f1d
zakeer <zakeer0412@gmail.com>
2022-09-07 11:16:04 +0530

    empty commmit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 
-import './app.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import About from './pages/About/About'
-import Contact from './pages/Contact/Contact'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import {Provider} from 'react-redux'
-import store from './redux/store'
+import './App.css'
 
 function App() {
-
-
   return (
-    <Provider store={store}>
     <div className='App'>
-      <Router>
-          <Routes>
-            <Route path='/' element={<Login  />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-      </Router>
+      Hello world
     </div>
-    </Provider>
   )
 }
 
